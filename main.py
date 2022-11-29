c633d37ef5299a82e793bb97f9aa71b8b6a76250
Atuka Beradze <114765308+Avtex19@users.noreply.github.com>
2022-11-29 14:11:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,33 +17,37 @@ class CapitalOfCountries:
         self.window.geometry("800x600")
         self.window.config(bg="black")
 
-        self.photoOne = PhotoImage(file="unnamed.png")
-        photoReduced = self.photoOne.subsample(2, 2)
-        self.photoOne_label = Label(self.window, image=photoReduced, bg="black")
-        self.photoOne_label.place(anchor=CENTER, relx=.5, rely=.23)
+        # self.photoOne = PhotoImage(file="unnamed.png")
+        # photoReduced = self.photoOne.subsample(2, 2)
+        # self.photoOne_label = Label(self.window, image=photoReduced, bg="black")
+        # self.photoOne_label.place(anchor=CENTER, relx=.5, rely=.23)
 
-        self.informationText = Label(self.window, fg='white', bg='black',
+        self.photoTwo = PhotoImage(file="world_map_PNG11.png")
+        self.photoTwo_label = Label(self.window, image=self.photoTwo)
+        self.photoTwo_label.place(x=0, y=0, relheight=1, relwidth=1)
+
+        self.informationText = Label(self.window, fg='black',
                                      text="Welcome to the our application. Please, enter the "
                                           "country which capital you want to know.",
                                      font=('arial', 18, 'bold'))
-        self.informationText.place(anchor=CENTER, relx=.5, rely=.5)
+        self.informationText.place(anchor=CENTER, x=400, y=30)
 
-        self.textEntry = Entry(self.window, width=30, bg='white', fg="black", font=('arial', 16, 'bold'))
-        self.textEntry.place(anchor=CENTER, relx=.5, rely=.6)
+        self.textEntry = Entry(self.window, width=30, bg="WhiteSmoke", fg="black", font=('arial', 16, 'bold'))
+        self.textEntry.place(anchor=CENTER, relx=.5, rely=.1)
 
-        self.click_button = Button(self.window, text='SUBMIT', command=self.click, width=14,font=('arial', 20,
-                                   'bold'),
+        self.click_button = Button(self.window, text='SUBMIT', command=self.click, width=14, font=('arial', 20,
+                                                                                                   'bold'),
                                    bg="white", fg="black")
-        self.click_button.place(anchor=CENTER, relx=.3, rely=.7)
+        self.click_button.place(anchor=CENTER, relx=.3, rely=.82)
 
-        self.output = Text(self.window, width=75, height=3, wrap=WORD, bg="white", fg="black",
+        self.output = Text(self.window, width=30, height=1.5, wrap=WORD,bg="WhiteSmoke",fg="black",
                            font=('arial', 16, 'bold'))
-        self.output.place(anchor=CENTER, relx=.5, rely=.85)
+        self.output.place(anchor=CENTER, relx=.5, rely=.9)
 
         self.exit_button = Button(self.window, text="EXIT", width=14, command=self.close_window, bg="white",
                                   font=('arial', 20, 'bold'),
                                   fg="black")
-        self.exit_button.place(anchor=CENTER, relx=.7, rely=.7)
+        self.exit_button.place(anchor=CENTER, relx=.7, rely=.82)
 
         self.window.mainloop()
 
